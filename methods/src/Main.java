b09ea2bbfb15a8be85d05e7ad28d38e74f8e16c3
Ertuğrul Garipardıç <118535200+ertugrulgaripardic@users.noreply.github.com>
2024-07-09 13:12:00 +0300

    Add files via upload

---
+++ methods/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+}
